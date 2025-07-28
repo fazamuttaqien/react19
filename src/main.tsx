8b0c48c2ed5de90997a00005f004ba47aded3218
Faza Muttaqien <fazamttqn@gmail.com>
2025-07-29 06:29:43 +0700

    feat(setup): setup react-router-v7

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router"
 
 import "./index.css"
-import App from "./App.tsx"
+// import App from "./App.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello from main page</h1>,
+  },
+])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
